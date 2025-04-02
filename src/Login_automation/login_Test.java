c4a58c4aacbc711230e6935a8ce7c45858368a5c
Suraj Humane <surajhum5@gmail.com>
2025-04-03 00:29:00 +0530

    Login method

---
+++ src/Login_automation/login_Test.java
@@ -0,0 +1,12 @@
+package Login_automation;
+
+public class login_Test {
+public void login_method() {
+	System.out.println("This is login method");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
