1e661eced9382ba31be1ca7f10970638e679f799
suraj swqa <surajhum5@gmail.com>
2025-04-03 01:03:11 +0530

    log out method

--- src/Login_automation/login_Test.java
+++ src/Login_automation/login_Test.java
@@ -4,6 +4,9 @@ public class login_Test {
 public void login_method() {
 	System.out.println("This is login method");
 }
+public void logout_method() {
+	System.out.println("This is logout method");
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
